8e3d9dc298ca02f2498d1ffc910c0784155e7aa1
TahiJameel <tahajameel.co@gmail.com>
2021-09-24 16:53:25 +0500

    connect mongodb

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("database connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
